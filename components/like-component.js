58749c746fcf96db18754e08886c3c4297bc0fcd
evgenkupriianov <kupriianovi@inbox.ru>
2023-12-18 18:03:05 +0300

    cw

---
+++ components/like-component.js
@@ -0,0 +1,43 @@
+import { getToken, renderApp, posts, setPosts } from "../index.js";
+import { setLike, removeLike, getUserPosts } from "../api.js";
+
+export const likeEventListener = () => {
+  const likeButtons = document.querySelectorAll(".like-button");
+
+  likeButtons.forEach(likeButton => {
+    likeButton.addEventListener("click", (event) => {
+      event.stopPropagation();
+      const postId = likeButton.dataset.postId;
+      const index = likeButton.dataset.index;
+      likeButton.classList.add("shake-bottom");
+
+      if (posts[index].isLiked) {
+        removeLike({ token: getToken(), postId })
+          .then(() => {
+            posts[index].isLiked = false;
+          })
+          .then(() => {
+            getPosts({ token: getToken() })
+              .then((response) => {
+                setPosts(response);
+                likeButton.classList.remove("shake-bottom");
+                renderApp();
+              })
+          })
+      } else {
+        setLike({ token: getToken(), postId })
+          .then(() => {
+            posts[index].isLiked = true;
+          })
+          .then(() => {
+            getPosts({ token: getToken() })
+              .then((response) => {
+                setPosts(response);
+                likeButton.classList.remove("shake-bottom");
+                renderApp();
+                })
+            })
+        }
+      });
+    });
+  };
