2c7a766b099a89d284456c07e56474788e5fc98e
evgenkupriianov <kupriianovi@inbox.ru>
2023-12-18 18:09:48 +0300

    cw

--- api.js
+++ api.js
@@ -1,11 +1,28 @@
-// Замени на свой, чтобы получить независимый от других набор данных.
-// "боевая" версия инстапро лежит в ключе prod
-const personalKey = "prod";
+const personalKeyMy = "eugene-sokolov";
+const personalKey = "prod"
 const baseHost = "https://webdev-hw-api.vercel.app";
 const postsHost = `${baseHost}/api/v1/${personalKey}/instapro`;
 
 export function getPosts({ token }) {
   return fetch(postsHost, {
+    method: "GET",
+    headers: {
+      Authorization: token,
+    },
+  })
+    .then((response) => {
+      if (response.status === 401) {
+        throw new Error("Нет авторизации");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      return data.posts;
+    });
+}
+
+export function getUserPosts({ token, userId }) {
+  return fetch(`${postsHost}/user-posts/${userId}`, {
     method: "GET",
     headers: {
       Authorization: token,
@@ -23,7 +40,60 @@ export function getPosts({ token }) {
     });
 }
 
-// https://github.com/GlebkaF/webdev-hw-api/blob/main/pages/api/user/README.md#%D0%B0%D0%B2%D1%82%D0%BE%D1%80%D0%B8%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D1%8C%D1%81%D1%8F
+export const addPost = ({ token, description, imageUrl }) => {
+  return fetch(postsHost, {
+    method: "POST",
+    body: JSON.stringify({
+      description,
+      imageUrl,
+    }),
+    headers: {
+      Authorization: token,
+    },
+  })
+    .then((response) => {
+      if (response.status === 400) {
+        alert("Выберите фото и добавьте комментарий");
+        throw new Error("Выберите фото и добавьте комментарий");
+      }
+
+      return response.json();
+    })  
+}
+
+export const setLike = ({ token, postId }) => {
+  return fetch(postsHost + '/' + postId + "/like", {
+    method: "POST",
+    headers: {
+      Authorization: token,
+    },
+  })
+    .then((response) => {
+      if (response.status === 401) {
+        alert("Нет авторизации")
+        throw new Error("Нет авторизации");
+      }
+
+      return response.json();
+    })
+}
+
+export const removeLike = ({ token, postId }) => {
+  return fetch(postsHost + '/' + postId + "/dislike", {
+    method: "POST",
+    headers: {
+      Authorization: token,
+    },
+  })
+    .then((response) => {
+      if (response.status === 401) {
+        throw new Error("Нет авторизации");
+      }
+
+      return response.json();
+    })
+}
+
 export function registerUser({ login, password, name, imageUrl }) {
   return fetch(baseHost + "/api/user", {
     method: "POST",
@@ -56,7 +126,6 @@ export function loginUser({ login, password }) {
   });
 }
 
-// Загружает картинку в облако, возвращает url загруженной картинки
 export function uploadImage({ file }) {
   const data = new FormData();
   data.append("file", file);
@@ -65,6 +134,7 @@ export function uploadImage({ file }) {
     method: "POST",
     body: data,
   }).then((response) => {
+    
     return response.json();
   });
 }
